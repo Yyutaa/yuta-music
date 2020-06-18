bc45a7b45b7f371f0eac01b22334ceeffbb9de93
panzonglin <1328785074@qq.com>
2020-06-18 18:21:57 +0800

    首次提交：初始化项目

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { IconStyle } from './assets/iconfont/iconfont';
+import { GlobalStyle } from  './style';
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle></GlobalStyle>
+      <IconStyle></IconStyle>
+      {/* <i className="iconfont">&#xe642;</i> */}
+    </div>
+  );
+}
+
+export default App;
