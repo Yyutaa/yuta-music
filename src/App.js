a2ea6e8d0a6aee7426306273653fb77596273800
yuta <254645467@qq.com>
2020-06-19 18:24:47 +0800

    创建redux状态管理

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import { GlobalStyle } from  './style';
 import { renderRoutes } from 'react-router-config';//renderRoutes 读取路由配置转化为 Route 标签
 import routes from './routes/index.js';
 import { HashRouter } from 'react-router-dom';
+import store from './store/index'
+import { Provider } from 'react-redux'
 
 function App() {
   return (
-    <HashRouter>
-      <div className="App">
-        <GlobalStyle></GlobalStyle>
-        <IconStyle></IconStyle>
-        { renderRoutes (routes) }
-      </div>
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <div className="App">
+          <GlobalStyle></GlobalStyle>
+          <IconStyle></IconStyle>
+          { renderRoutes (routes) }
+        </div>
+      </HashRouter>
+    </Provider>
   );
 }
 
